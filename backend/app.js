7b96c3fce6a85fbeb7aff7bce39a3cc4e200a1e4
Phan Tri <minhtritt01@gmail.com>
2022-10-11 21:30:50 +0700

    ok roi do

--- backend/app.js
+++ backend/app.js
@@ -30,11 +30,9 @@ app.use('/api/v2', payment)
 
 app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-app.get('*', (req, res) =>
-  res.sendFile(
-    path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-  )
-)
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+})
 
 //it's for error handling
 app.use(ErrorHandler)
