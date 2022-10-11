dbc2c66eeadf67c1eba3c57d89918c5edebc38a7
Phan Tri <minhtritt01@gmail.com>
2022-10-11 21:04:38 +0700

    port

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ cloudinary.config({
 })
 
 //create server
-const server = app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`)
 })
 //unhandled promise rejection
