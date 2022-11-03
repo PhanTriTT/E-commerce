fc9623d64b825bcb9217c04bab5c66e72e78fdf0
Phan Tri <minhtritt01@gmail.com>
2022-11-03 14:39:38 +0700

    fix sort price

--- frontend/src/actions/ProductActions.js
+++ frontend/src/actions/ProductActions.js
@@ -39,7 +39,7 @@ export const getProduct =
       let link = `/api/v2/products?keyword=${keyword}&page=${currentPage}`
 
       if (category) {
-        link = `/api/v2/products?keyword=${keyword}&page=${currentPage}&category=${category}&sort=${sort}`
+        link = `/api/v2/products?keyword=${keyword}&page=${currentPage}&category=${category}`
       } else if (sort) {
         link = `/api/v2/products?sort=${sort}`
       }
